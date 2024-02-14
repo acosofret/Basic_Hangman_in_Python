b0b0d691c13d3bfc6712f273554f5c1f0c7bc351
acosofret <acosofretcwst@gmail.com>
2024-02-14 07:24:24 +0000

    Game is now fully functional. Further user experience improvemenets can be added by clearing the screen after each stage of the game, but the code was written in PyCharm which seems to have limited functionality and a clear function cannot be performed. Hoever thsi should be easily done using other environment, simply importing replit and using clear() function

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
-#Step 1
+# you can improve user experience by clearing the screen on each stage of the game
+# # you can do it by importing <from replit import clear> and then using <clear()> function
+# # but I used PyCharm to write the code, and it doesn't work in pycharm
 import random
-
-word_list = ["aardvark", "baboon", "cammel"]
+from game_art import logo, stages
+from game_words import word_list
 
 word_to_guess = random.choice(word_list)
 # Printing the word to ease the code writing:
 print(f"(<<< CODE Help: Pssst, the solution is '{word_to_guess}'.>>>)")
 
-# Display the word to be guessed with the characters hidden:
+# Define a variable to keep track of the player's lives:
+lives = 6
 
+# Ask user to choose a letter:
+# Add it in a loop, so it asks for input until all the letters are guessed
+# Display the stages so user can see lives left
+# Display the word to be guessed with the characters hidden:
 word_on_display_list = []
 for letter in word_to_guess:
 	word_on_display_list += "_"
 word_on_display = ''.join(word_on_display_list)
 
-print(word_on_display)
+end_of_game = False
+while not end_of_game:
+	if lives > 0:
+		print(logo)
+		print(stages[lives])
+		print(word_on_display)
+		guess = input("Please enter a letter: ").lower()
 
-# Ask user to choose a letter:
-guess = input("Please enter a letter:\n").lower()
+	# check if the chosen letter is in the word to be guessed:
+	# if the letter is in the word then replace the respective hidden character in displayed word.
+	# if letter is not in the word then lives goes down by 1
+		n = 0
+		if guess in word_on_display:
+			print(f"you already guessed '{guess}'")
+		if guess in word_to_guess:
+			print("Well done.")
+			for letter in word_to_guess:
+				n += 1
+				if letter == guess:
+					word_on_display_list[n-1] = letter
+			word_on_display = ''.join(word_on_display_list)
 
-# check if the chosen letter is in the word to be guessed:
-# if the letter is in the word then replace the respective hidden character in displayed word.
-n = 0
-for letter in word_to_guess:
-	n += 1
-	if letter == guess:
-		word_on_display_list[n-1] = letter
-word_on_display = ''.join(word_on_display_list)
-
-print(word_on_display)
-
-
-# _____________THIS IS WHERE I AM LEFT________________________________________
-
-# Check if the user has won: #75
+			if "_" not in word_on_display:
+				end_of_game = True
+				print("You won!")
+		else:
+			lives -= 1
+			print(f"Sorry! '{guess}' is not in the word.")
+	else:
+		print("You lost ...")
+		end_of_game = True
